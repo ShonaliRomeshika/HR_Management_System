2c8633e1447e1d6e300787a495baf76601f6561c
ShonaliRomeshika <shonaliromeshika@gmail.com>
2025-08-19 23:25:29 +0530

    initial commit

---
+++ hr-backend/src/test/java/com/hdsr/hr/HrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdsr.hr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
