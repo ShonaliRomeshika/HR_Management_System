8ec794cbfcef11d4f911c1e5532431bec83bc5ed
ShonaliRomeshika <shonaliromeshika@gmail.com>
2025-09-26 15:44:31 +0530

    created department-list page

--- hr-frontend/src/app/app-routing.module.ts
+++ hr-frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/home/home.component';
 import { DesignationCreateComponent } from './designation/designation-create/designation-create/designation-create.component';
 import { DesignationListComponent } from './designation/designation-list/designation-list/designation-list.component';
 import { DepartmentCreateComponent } from './department/department-create/department-create/department-create.component';
+import { DepartmentListComponent } from './department/department-list/department-list/department-list.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'designations', component: DesignationListComponent },
   { path: 'designations/create', component: DesignationCreateComponent },
   { path: 'designations/create/:id', component: DesignationCreateComponent },
+  { path: 'departments', component: DepartmentListComponent },
   { path: 'departments/create', component: DepartmentCreateComponent },
   { path: 'departments/create/:id', component: DepartmentCreateComponent },
 ];
--- hr-frontend/src/app/department/department-list/department-list/department-list.component.ts
+++ hr-frontend/src/app/department/department-list/department-list/department-list.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Department, DepartmentService } from '../../department.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog/confirm-dialog.component';
+
 
 @Component({
   selector: 'app-department-list',
   templateUrl: './department-list.component.html',
   styleUrls: ['./department-list.component.css']
 })
-export class DepartmentListComponent implements OnInit {
 
-  constructor() { }
+export class DepartmentListComponent implements OnInit {
+  departments: Department[] = [];
+
+  constructor(
+    private service: DepartmentService, 
+    private router: Router,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
+    this.loadDepartments();
   }
 
+  addDepartment() {
+    this.router.navigate(['/departments/create']);
+  }
+
+  updateDepartment(id: string) {
+    this.router.navigate(['/departments/create', id]);
+  }
+
+ loadDepartments() {
+  const token = localStorage.getItem('token')!; 
+  this.service.getAll(token).subscribe(data => this.departments = data);
 }
+
+deleteDepartment(id: string, name?: string) {
+  const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+    width: '350px',
+    data: { item: name || 'this department' }
+  });
+
+  dialogRef.afterClosed().subscribe(result => {
+    if (result) {
+      const token = localStorage.getItem('token')!;
+      this.service.delete(id, token).subscribe(() => this.loadDepartments());
+    }
+  });
+}
+
+}
+
+
